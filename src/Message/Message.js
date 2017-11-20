2f8d3dea766de9a8f65b6d96cc0e4d11eb515cd0
Gal Dubitski <galmail@gmail.com>
2017-11-20 00:11:55 +0000

    Displaying and formating the timestamp of the message to be human readable

--- src/Message/Message.js
+++ src/Message/Message.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import moment from 'moment';
+
 import Avatar from '../Avatar';
 
 import './Message.css';
@@ -7,8 +9,9 @@ const Message = ({ id, userId, message, timestamp, user }) => (
     <div className="message">
         <Avatar imageUrl={user && user.avatar} imageName={getName(user)} />
         <div className="message-body">
-            {message}
             <span className="tooltip">{user && user.email}</span>
+            <div className="text">{message}</div>
+            <div className="time">{moment(timestamp).format('LLL')}</div>
         </div>
     </div>
 );
